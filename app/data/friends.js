8a80f0670a3f9ee07427b02829f427168de6c205
Mason Stigall <masonstigall@gmail.com>
2018-08-13 11:46:15 -0400

    added actual survey questions

--- app/data/friends.js
+++ app/data/friends.js
@@ -28,12 +28,77 @@ var friendsArray = [
             5,
             4,
             2,
-            3
+            5
         ]
     },
     {
         name: "Christmas Cat",
         photo: "https://i.imgur.com/6sG6aJm.png",
+        scores: [
+
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            1
+        ]
+    },
+    {
+        name: "Snuggly Dog Duo",
+        photo: "https://i.imgur.com/u1A5DkN.jpg",
+        scores: [
+            2,
+            1,
+            3,
+            4,
+            5,
+            5,
+            3,
+            4,
+            2,
+            5
+        ]
+    },
+    {
+        name: "Upside Down Dog",
+        photo: "https://i.imgur.com/mFcMDJf.jpg",
+        scores: [
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5
+        ]
+    },
+    {
+        name: "Fetch Aficionado",
+        photo: "https://i.imgur.com/UIFWQhX.jpg",
+        scores: [
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            4
+        ]
+    },
+    {
+        name: "Happy Grass Dog",
+        photo: "https://i.imgur.com/dBttKNt.jpg",
         scores: [
             5,
             1,
@@ -44,7 +109,7 @@ var friendsArray = [
             3,
             5,
             1,
-            1,
+            5,
         ]
     }
 ]
